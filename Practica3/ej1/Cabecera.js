97d3e6b29c8a15e27b5f1e82474b75a20f37c9fb
Ignacio Gómez Gasch <75808025+UO271548@users.noreply.github.com>
2021-11-23 11:20:17 +0100

    Initial Commit

---
+++ Practica3/ej1/Cabecera.js
@@ -0,0 +1,9 @@
+var cabeceraAsignatura = new Object();
+
+cabeceraAsignatura.nombre = "Software y Estándares para la web";
+cabeceraAsignatura.nombreTitulacion = "Ingenería Informática del Software";
+cabeceraAsignatura.nombreCentro = "Escuela de Ingeniería Informática";
+cabeceraAsignatura.nombreUniversidad = "Universidad de Oviedo";
+cabeceraAsignatura.cursoActual = "3º";
+cabeceraAsignatura.nombreEstudiante = "Ignacio Gómez Gasch";
+cabeceraAsignatura.email = "uo271548@uniovi.es";
